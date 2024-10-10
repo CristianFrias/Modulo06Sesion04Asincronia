8eb7c780f3e0efd7b4f72857c0aaf9ba72fb243b
Cristián Andrés Frías García <cristianfrias@MacBook-Air-de-Cristian.local>
2024-10-09 23:29:15 -0300

    Ejercicio de consumo de API y escritura en archivos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// import { consultarPersonaje } from "./utils.js";
-import { writeFile, appendFile, readFile } from "fs"; // fs es una dependencia nativa de Node.js
+import { consultarPersonaje } from "./utils.js";
+import { writeFile, appendFile, readFile, unlink, readFileSync, writeFileSync } from "fs"; // fs es una dependencia nativa de Node.js
 
 (async () => {
     // Ejemplo de proceso asíncrono
@@ -16,13 +16,26 @@ import { writeFile, appendFile, readFile } from "fs"; // fs es una dependencia n
     // }
 
     // Ejemplo de Promise.all
-    // const promesas = []
-    // for (let index = 1; index <= 10; index++) {
-    //     const peticion = consultarPersonaje(index)
-    //     promesas.push(peticion)
-    // }
-    // const response = await Promise.all(promesas)
-    // console.log(response);
+    const promesas = []
+    for (let index = 11; index <=20; index++) {
+        const peticion = consultarPersonaje(index)
+        promesas.push(peticion)
+    }
+    const responses = await Promise.allSettled(promesas)
+
+    const contenidoString = readFileSync("files/personajes.txt","utf-8")
+    const contenidoObject = JSON.parse(contenidoString)
+    
+    for (const resp of responses) {
+        if(resp.status === 'rejected') {
+            continue;
+        } 
+        const { name, height, mass } = resp.value
+        const personaje = {name, height, mass}
+        contenidoObject.push(personaje)
+    }
+
+    writeFileSync("files/personajes.txt", JSON.stringify(contenidoObject),"utf-8")
 
     // Escritura en archivo sustituyendo lo ya existente
     // writeFile("files/personas.txt", "Texto de Prueba", "utf-8", (data) => console.log(data))
@@ -31,11 +44,16 @@ import { writeFile, appendFile, readFile } from "fs"; // fs es una dependencia n
     // appendFile("files/personas.txt", "\n\nTexto de Prueba", "utf-8", (data) => console.log(data))
 
     // Ejemplo de lectura de archivo
-    readFile("files/personas.txt", "utf-8", (error, data) => {
-        if (error) {
-            console.log("Ocurrio un error en la lectura");
-        } else {
-        console.log(data);
-        }
-    })
+    // readFile("files/personas.txt", "utf-8", (error, data) => {
+    //     if (error) {
+    //         console.log("Ocurrio un error en la lectura");
+    //     } else {
+    //     console.log(data);
+    //     }
+    // })
+
+    // Ejemplo de eliminación de archivo
+    // unlink ("files/personas.txt", () => {
+    //     console.log("Archivo eliminado");
+    // })
 })()
